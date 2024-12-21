295bf621f4afd6207598f1b7b26490eb4086f08e
oleole <ooa64@ua.fm>
2024-12-21 20:56:21 +0200

    added dbf object with one command - forget

--- generic/tcldbfcmd.hpp
+++ generic/tcldbfcmd.hpp
@@ -2,13 +2,14 @@
 #define TCLDBFCMD_H
 
 #include <tcl.h>
-/*#include <shapefil.h>*/
+#include <shapefil.h>
 #include "tclcmd.hpp"
 
 class TclDbfCmd : public TclCmd {
 public:
   TclDbfCmd(Tcl_Interp * interp, const char * name): TclCmd(interp, name) {};
-private:    
+private:
+  int dbfcounter = 0;    
   virtual int Command(int objc, Tcl_Obj * const objv[]);
   virtual void Cleanup();
 };
