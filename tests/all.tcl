2aa3ff5315bc86874eaeca72b5a0eb1195d06394
ooa64 <ooa64@ua.fm>
2025-02-24 14:14:55 +0200

    fixed tests for encoding features

--- tests/all.tcl
+++ tests/all.tcl
@@ -4,4 +4,10 @@ package require tcltest 2.2
 ::tcltest::configure \
 	-testdir [file dirname [file normalize [info script]]] \
 	{*}$argv
+
+puts "Tests auto_path:  $auto_path"
+puts "Tests encoding system:  [encoding system]"
+puts "Tests package version:  [package require tcltest]"
+puts "Tests interpreter version:  [info patchlevel]"
+
 ::tcltest::runAllTests
